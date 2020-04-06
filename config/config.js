3fa9b1618e1af7c06fe0a43e84f4528d44127041
hariszaki17 <hariszaki17@gmail.com>
2020-04-06 21:01:57 +0700

    testing config 2

--- config/config.js
+++ config/config.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 
 module.exports = {
-  "development": {
-    "username": "postgres",
-    "password": "StrongAdminP@ssw0rd",
-    "database": "db_todo",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
-  },
+  // "development": {
+  //   "username": "postgres",
+  //   "password": "StrongAdminP@ssw0rd",
+  //   "database": "db_todo",
+  //   "host": "127.0.0.1",
+  //   "dialect": "postgres"
+  // },
   "production": {
     "username": process.env.DATABASE_USER,
     "password": process.env.DATABASE_PASSWORD,
