a67e71205f66970f239b116a21e4ca43b5b4ef5e
hariszaki17 <hariszaki17@gmail.com>
2020-04-07 19:44:14 +0700

    socket trial

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 const PORT = process.env.PORT
 const routers = require('./routers')
 const cors = require('cors')
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 const errorhandler = require('./middlewares/errorhandler')
 
 app.use(cors())
@@ -11,6 +13,15 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use('/', routers)
 app.use(errorhandler)
+
+io.on('connection', (socket) => {
+    socket.broadcast.emit('hai')
+    console.log('a user connected')
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+      });
+})
+
 app.listen(PORT, () => {
     console.log('I love u ', PORT, new Date())
 })
